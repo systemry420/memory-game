6068cc259187cb7e9f5603e9368ed1310eecef17
Hasan <systemry420@gmail.com>
2018-12-30 20:56:58 +0200

    add flip animations and check for matches

--- js/app.js
+++ js/app.js
@@ -1,14 +1,45 @@
+// dceclare variables
+let deck = document.querySelector(".deck");
+let opened = [];
+let matches = 0;
+let moves = document.querySelector('#moves');
+let move = 0;
 /*
  * Create a list that holds all of your cards
  */
 
+let cards = ['fa-diamond', 'fa-diamond',
+             'fa-paper-plane-o', 'fa-paper-plane-o',
+             'fa-anchor', 'fa-anchor',
+             'fa-bolt', 'fa-bolt',
+             'fa-cube', 'fa-cube',
+             'fa-leaf', 'fa-leaf',
+             'fa-bicycle', 'fa-bicycle',
+             'fa-bomb', 'fa-bomb'
+            ];
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+* Display the cards on the page
+*   - shuffle the list of cards using the provided "shuffle" method below
+*   - loop through each card and create its HTML
+*   - add each card's HTML to the page
+*/
+
+shuffle(cards);
+
+function startGame() {
+    let grid = cards.map(function(card){
+        return generatCards(card);
+    });
+}
+
+function generatCards(card) {
+    let att = card.slice(3);
+    let cardTemp = `<li class="card" data-icon="${att}"><i class="fa ${card}"></i></li>`;
+    deck.innerHTML += cardTemp;
+}
+
+startGame();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,3 +67,44 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+deck.addEventListener('click', flip);
+
+function flip(e) {
+    let element = e.target;
+    let node = e.target.nodeName.toLowerCase();
+    if(node == 'li'){
+        moves.innerHTML = move++;
+        opened.push(element);
+        element.classList.add('open', 'show');
+        if(opened.length == 2){
+            // check match
+            if(checkMatch(opened)){
+                // TODO: pop
+                opened[0].classList.add('match');
+                opened[1].classList.add('match');
+                opened = [];
+                matches++;
+            }
+            else{
+                // TODO: shake
+                opened[0].classList.add('unmatch');
+                opened[1].classList.add('unmatch');
+                setTimeout(() => {
+                    opened[0].classList.remove('open', 'show', 'unmatch');
+                    opened[1].classList.remove('open', 'show', 'unmatch');
+                    opened = [];
+                }, 1000);
+            }
+        }
+    }
+    if(matches == 6){
+        alert("finish")
+    }
+}
+
+function checkMatch(arr) {
+    if(arr[0].dataset.icon === arr[1].dataset.icon){
+        return true;
+    }
+    return false;
+}
