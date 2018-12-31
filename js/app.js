9ac0f0f4cd0a3953223000b60cd0047658c4b9c5
Hasan <systemry420@gmail.com>
2018-12-31 23:10:33 +0200

    add more logic: match, unmatch, stars, timer, restart, gameover ..

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 // dceclare variables
 let deck = document.querySelector(".deck");
 let opened = [];
-let matches = 0;
+let matches = [];
 let moves = document.querySelector('#moves');
-let move = 0;
+let move, wrongMoves = 0;
+let restart = document.querySelector("#restart");
+let time = 0;
+let duration = document.querySelector("#duration");
+let play = document.querySelector('#play');
+let modal = document.querySelector('.modal');
+let stars = document.querySelectorAll('.stars li');
+let s = 4;
+let click = 0;
+let x;
+
+// restart game
+restart.addEventListener('click', startGame);
+
+deck.addEventListener('click', flip);
+
 /*
  * Create a list that holds all of your cards
  */
@@ -25,10 +40,28 @@ let cards = ['fa-diamond', 'fa-diamond',
 *   - add each card's HTML to the page
 */
 
-shuffle(cards);
 
 function startGame() {
-    let grid = cards.map(function(card){
+    // reset variables
+    clearInterval(x);
+    click = time = move = wrongMoves = 0;
+    matches = [];
+    opened = [];
+    s = 4;
+    deck.innerHTML = "";
+    moves.innerHTML = move;
+    duration.innerHTML = 0;
+    deck.style.opacity = '1';
+    modal.style.display = 'none';
+    stars.forEach(st => {
+        st.style.color = '#000';
+    });
+
+    deck.addEventListener('click', flip);
+
+    // shuffle and generate cards
+    shuffle(cards);
+    cards.forEach(function(card){
         return generatCards(card);
     });
 }
@@ -56,6 +89,10 @@ function shuffle(array) {
     return array;
 }
 
+play.addEventListener('click', function(){
+    startGame();
+    modal.style.display = 'none';
+});
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -67,26 +104,45 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-deck.addEventListener('click', flip);
 
 function flip(e) {
     let element = e.target;
+    ++click;
     let node = e.target.nodeName.toLowerCase();
+    if(click == 1){
+        x = setInterval(timer, 1000);
+    }
     if(node == 'li'){
-        moves.innerHTML = move++;
+        moves.innerHTML = ++move;
         opened.push(element);
         element.classList.add('open', 'show');
         if(opened.length == 2){
             // check match
             if(checkMatch(opened)){
-                // TODO: pop
                 opened[0].classList.add('match');
                 opened[1].classList.add('match');
+                matches.push(opened[0]);
+                matches.push(opened[1]);
                 opened = [];
-                matches++;
             }
             else{
-                // TODO: shake
+                wrongMoves+=2;
+                if(wrongMoves == 16){
+                    stars[s--].style.color = '#aaa';
+                }
+                else if(wrongMoves == 22){
+                    stars[s--].style.color = '#aaa';
+                }
+                else if(wrongMoves == 26){
+                    stars[s--].style.color = '#aaa';
+                }
+                else if(wrongMoves == 30){
+                    stars[s--].style.color = '#aaa';
+                }
+                else if(wrongMoves == 34){
+                    stars[s--].style.color = '#aaa';
+                    gameOver();
+                }
                 opened[0].classList.add('unmatch');
                 opened[1].classList.add('unmatch');
                 setTimeout(() => {
@@ -97,8 +153,8 @@ function flip(e) {
             }
         }
     }
-    if(matches == 6){
-        alert("finish")
+    if(matches.length == 16){
+        gameOver();
     }
 }
 
@@ -107,4 +163,34 @@ function checkMatch(arr) {
         return true;
     }
     return false;
+}
+
+function timer() {
+    time++;
+    duration.innerHTML = time;
+}
+
+function gameOver() {
+    // show modal
+    let mod = '';
+    clearInterval(x);
+    if(s >= 0 ){
+        mod = `
+        <i class="fa fa-rocket" style="color:green; font-size: 40px;"></i>
+        <h3>Congratulations! You won!</h3>
+        <p>Your time: <strong>${time}</strong> seconds</p>
+        <p>With <strong>${move}</strong> moves and <strong>${++s}</strong> star(s).</p>`;
+    }
+    else if(s < 0){
+        mod = `
+        <i class="fa fa-rocket" style="color:red; font-size: 40px;"></i>
+        <h3>Bad luck! You loose!</h3>
+        <p>Your time: <strong>${time}</strong> seconds</p>
+        <p>With <strong>${move}</strong> moves and <strong>${++s}</strong> star(s).</p>`;
+    }
+    let  modalBody = document.querySelector('#modal-body');
+    modalBody.innerHTML = mod;
+    deck.style.opacity = '0.6';
+    modal.style.display = 'block';
+    deck.removeEventListener('click', flip);
 }
